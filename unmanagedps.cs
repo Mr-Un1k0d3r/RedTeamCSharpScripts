bd36dc23e3e7a895df94a22fb7102e968e7f4d47
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2020-10-26 11:43:17 -0400

    Create unmanagedps.cs

---
+++ unmanagedps.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Management.Automation.Runspaces;
+
+namespace unmanagedps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                Runspace rs = RunspaceFactory.CreateRunspace();
+                rs.Open();
+
+                RunspaceInvoke ri = new RunspaceInvoke(rs);
+
+                Pipeline pipe = rs.CreatePipeline();
+                pipe.Commands.AddScript(args[0]);
+                pipe.Commands.Add("Out-String");
+                Collection<PSObject> output = pipe.Invoke();
+                rs.Close();
+                foreach (PSObject line in output)
+                {
+                    sb.AppendLine(line.ToString());
+                }
+                Console.WriteLine(sb.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
